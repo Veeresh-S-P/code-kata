a1bab28719ad57d218a56485a99085d5cd7a02f5
veeresh-s_p <veereshsp1234@gmail.com>
2023-09-04 17:37:38 +0530

    completed whole assignment of demyst i.e loan management application

---
+++ client/src/navigation/Routes.js
@@ -0,0 +1,7 @@
+export const Route = {
+    home: '/',
+    loanDetails: '/loan-details',
+    viewDetails: '/view-details',
+    outcome: '/outcome'
+  };
+  
---
+++ client/src/pages/LoanDetails/styles.js
@@ -0,0 +1,21 @@
+import {
+    contentStyle as landingPageContentStyle,
+    headerStyle as landingPageHeaderStyle,
+    headerText as landingPageheaderText
+  } from '../LandingPage/styles';
+  
+  export const headerStyle = {
+    ...landingPageHeaderStyle,
+    height: 'auto',
+    paddingTop: 0
+  };
+  
+  export const headerText = {
+    ...landingPageheaderText,
+    textAlign: 'center'
+  };
+  export const contentStyle = {
+    ...landingPageContentStyle,
+    justifyContent: 'center'
+  };
+  
